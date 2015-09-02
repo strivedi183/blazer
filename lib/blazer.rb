a361e846145a437e306284406d494eaa7a845c3a
Sandip Trivedi <trivedi.sandip@gmail.com>
2015-09-01 22:22:00 -0400

    Autosave query in the editor to local storage
    Added the ability to autosave query in the editor to local storage
    as the user types it and the ability to restore it and it is
    disabled by default but can be enabled by use_local_storage method

--- lib/blazer.rb
+++ lib/blazer.rb
@@ -10,10 +10,12 @@ module Blazer
     attr_accessor :user_name
     attr_accessor :user_class
     attr_accessor :timeout
+    attr_accessor :use_local_storage
   end
   self.audit = true
   self.user_name = :name
   self.timeout = 15
+  self.use_local_storage = false
 
   def self.time_zone=(time_zone)
     @time_zone = time_zone.is_a?(ActiveSupport::TimeZone) ? time_zone : ActiveSupport::TimeZone[time_zone.to_s]
